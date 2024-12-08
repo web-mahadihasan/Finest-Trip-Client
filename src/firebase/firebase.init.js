a675b42fed8bcb516b405e65d98a961bd530f843
web-mahadihasan <mehedihasanmilu7@gmail.com>
2024-12-08 23:25:39 +0600

    secure env file data

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBqlo7F-cxYi75l6UrMQi0cjmlqvyUe_i4",
-  authDomain: "finest-trip.firebaseapp.com",
-  projectId: "finest-trip",
-  storageBucket: "finest-trip.firebasestorage.app",
-  messagingSenderId: "490477160691",
-  appId: "1:490477160691:web:fd72a5d5e24ba72f294f86"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
